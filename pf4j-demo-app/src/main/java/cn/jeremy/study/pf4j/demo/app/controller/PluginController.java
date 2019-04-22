ca66bab29d8d0d7d22fa34ff68a4ffe28193b7fb
冯江涛 <fengjiangt@139.com>
2019-04-22 17:15:21 +0800

    集成插件管理页面

--- pf4j-demo-app/src/main/java/cn/jeremy/study/pf4j/demo/app/controller/PluginController.java
+++ pf4j-demo-app/src/main/java/cn/jeremy/study/pf4j/demo/app/controller/PluginController.java
@@ -4,72 +4,111 @@ import cn.jeremy.study.pf4j.demo.api.AppDevice;
 import cn.jeremy.study.pf4j.demo.api.domain.AppRequest;
 import org.apache.commons.lang.StringUtils;
 import org.pf4j.PluginManager;
+import org.pf4j.PluginWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.util.List;
 
-@RestController
+@Controller
 public class PluginController {
     private static Logger logger = LoggerFactory.getLogger(PluginController.class);
 
     @Autowired
     PluginManager pluginManager;
 
-    @RequestMapping("/find")
-    public String find(String appId, String text) {
-        List<AppDevice> extensions = pluginManager.getExtensions(AppDevice.class);
+    @RequestMapping("/index")
+    public String index(Model model) {
+        loadPlugins(model);
+        return "index";
+    }
 
+    @RequestMapping("/find")
+    public String find(String appId, String text, Model model) {
+
+        model.addAttribute("pluginResponse", "设备不存在：" + appId);
+        model.addAttribute("appId", appId);
+        model.addAttribute("text", text);
+        List<AppDevice> extensions = pluginManager.getExtensions(AppDevice.class);
         AppRequest request = new AppRequest(appId, text);
         for (AppDevice extension : extensions) {
             if (extension.getId().equals(appId)) {
-                return extension.getResponse(request);
+                model.addAttribute("pluginResponse", extension.getResponse(request));
             }
         }
-        return "设备不存在：" + appId;
+        loadPlugins(model);
+        return "index";
     }
 
     @RequestMapping("/load")
-    public String load(String fileName) {
-
+    public String load(String fileName, Model model) {
+        model.addAttribute("fileName", fileName);
         if (StringUtils.isEmpty(fileName)) {
-            return "fileName 不能为空";
+            model.addAttribute("loadPluginId", "fileName 不能为空");
+            loadPlugins(model);
+            return "index";
         }
-        String filePath = pluginManager.getPluginsRoot().toString().concat("/").concat(fileName);
-        return pluginManager.loadPlugin(new File(filePath).toPath());
+        String filePath = pluginManager.getPluginsRoot().toString().concat("/").concat(fileName.trim());
+        model.addAttribute("loadPluginId", pluginManager.loadPlugin(new File(filePath).toPath()));
+        loadPlugins(model);
+        return "index";
     }
 
     @RequestMapping("/unload")
-    public String unload(String pluginId) {
-        return String.valueOf(pluginManager.unloadPlugin(pluginId));
+    public String unload(String pluginId, Model model) {
+        model.addAttribute("unloadPluginId", pluginId);
+        pluginManager.unloadPlugin(pluginId);
+        loadPlugins(model);
+        return "index";
     }
 
     @RequestMapping("/enable")
-    public String enable(String pluginId) {
-        return String.valueOf(pluginManager.enablePlugin(pluginId));
+    public String enable(String pluginId, Model model) {
+        model.addAttribute("enablePluginId", pluginId);
+        pluginManager.enablePlugin(pluginId);
+        loadPlugins(model);
+        return "index";
     }
 
     @RequestMapping("/disable")
-    public String disable(String pluginId) {
-        return String.valueOf(pluginManager.disablePlugin(pluginId));
+    public String disable(String pluginId, Model model) {
+        model.addAttribute("disablePluginId", pluginId);
+        pluginManager.disablePlugin(pluginId);
+        loadPlugins(model);
+        return "index";
     }
 
     @RequestMapping("/start")
-    public String start(String pluginId) {
-        return String.valueOf(pluginManager.startPlugin(pluginId));
+    public String start(String pluginId, Model model) {
+        model.addAttribute("startPluginId", pluginId);
+        pluginManager.startPlugin(pluginId);
+        loadPlugins(model);
+        return "index";
     }
 
     @RequestMapping("/stop")
-    public String stop(String pluginId) {
-        return String.valueOf(pluginManager.stopPlugin(pluginId));
+    public String stop(String pluginId, Model model) {
+        model.addAttribute("stopPluginId", pluginId);
+        pluginManager.stopPlugin(pluginId);
+        loadPlugins(model);
+        return "index";
     }
 
     @RequestMapping("/delete")
-    public String delete(String pluginId) {
-        return String.valueOf(pluginManager.deletePlugin(pluginId));
+    public String delete(String pluginId, Model model) {
+        model.addAttribute("deletePluginId", pluginId);
+        pluginManager.deletePlugin(pluginId);
+        loadPlugins(model);
+        return "index";
+    }
+
+    private void loadPlugins(Model model) {
+        List<PluginWrapper> plugins = pluginManager.getPlugins();
+        model.addAttribute("plugins", plugins);
     }
 }
